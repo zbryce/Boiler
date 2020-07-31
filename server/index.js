92fdea5518aa96f4742eab4e878c270b09314671
Zach Bryce <zachbryce11@gmail.com>
2020-07-31 14:09:17 -0500

    fixed error handlers

--- server/index.js
+++ server/index.js
@@ -13,14 +13,35 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api', require('./api'))
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
+
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
 app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
-app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+
 
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
 app.listen(port, function () {
